1c17bc77b9bad25b5c3697c20d513c585fcce5e1
Joe <spizjo01@gmail.com>
2022-09-22 14:48:27 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The future home of Joe Spizzandre's Portfolio
         </p>
         <a
           className="App-link"
