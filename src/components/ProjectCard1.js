d31eddda0b737014c83e6a2260402e05038b76c3
Joe <spizjo01@gmail.com>
2022-11-07 16:41:09 -0700

    done for the day. working on project cards

--- src/components/ProjectCard1.js
+++ src/components/ProjectCard1.js
@@ -5,21 +5,22 @@ function ProjectCard1() {
   const ref = useRef(null);
   const isInView = useInView(ref);
     let motionRules = {
-        transform: isInView ? "none" : "translateX(-200px)",
+        transform: isInView ? "none" : "translateY(200px)",
         opacity: isInView ? 1 : 0,
         transition: "2s"
       }
   return (
-    <div ref={ref}
-    ><div  className="projectCard" style={motionRules}>
+    <div ref={ref} className="projectCard" 
+    ><div  style={motionRules}>
       <div>
+        <img className='projectGIF' src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-10/thumbs-up-cz-2x1-221013-fe4a69.jpg" alt="project GIF"/>
     <h1 className='sectionHead'>What are we doing tonight?</h1>
     <p>A small front-end App built after only 2 months of part-time programming. JS, HTML, CSS</p>
 <a href="https://plan4tonight.netlify.app/" target="_blank" rel="noreferrer">Plan for Tonight</a>
 </div>
-<img className="techSymbol"  title="HTML" src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png" alt="HTML"/>
-<img className="techSymbol" src="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png" alt="javascript logo"/>
-<img lassName="techSymbol"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS logo"/>
+<img className="projectSymbol"  title="HTML" src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png" alt="HTML"/>
+<img className="projectSymbol" src="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png" alt="javascript logo"/>
+<img className="projectSymbol"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS logo"/>
                                        
 </div></div>
   )
--- src/components/ProjectCard2.js
+++ src/components/ProjectCard2.js
@@ -5,22 +5,23 @@ function ProjectCard2() {
     const ref = useRef(null);
   const isInView = useInView(ref);
     let motionRules = {
-        transform: isInView ? "none" : "translateX(200px)",
+        transform: isInView ? "none" : "translateY(200px)",
         opacity: isInView ? 1 : 0,
         transition: "2s"
       }
   return (
-    <div ref={ref}>
-        <div className="projectCard" style={motionRules}>
+    <div ref={ref} className="projectCard" >
+        <div style={motionRules}>
             <div>
+            <img className='projectGIF' src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-10/thumbs-up-cz-2x1-221013-fe4a69.jpg" alt="project GIF"/>
           <h1 className='sectionHead'>A Purring Wolverine Website</h1>
           <p> I made a  website for my own personal YouTube Page!</p>
           <a href="https://apurringwolverine.netlify.app/"target="_blank" rel="noreferrer">A Purring Wolverine</a>
           </div>
-          <img className="techSymbol"  title="HTML" src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png" alt="HTML"/>
-    <img className="techSymbol" src="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png" alt="javascript logo"/>
-    <img lassName="techSymbol"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS logo"/>
-    <img className="techSymbol" src="https://www.pngfind.com/pngs/m/685-6854994_react-logo-no-background-hd-png-download.png" alt="React Logo"/>
+          <img className="projectSymbol"  title="HTML" src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png" alt="HTML"/>
+    <img className="projectSymbol" src="https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png" alt="javascript logo"/>
+    <img className="projectSymbol"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS logo"/>
+    <img className="projectSymbol" src="https://png2.cleanpng.com/sh/643d56e06c1a1a1a0738849fb4ebeda3/L0KzQYm3VsA1N5p0iZH0aYP2gLBuTgJmaZR5RdxqdnH2c8PwkQQuaZ9sjd5qcnr2Pbr2jvlkNZJ5h982NXK0RYXphcY6PGg5Tak3M0S6QYq7UcgyPWM9SaQ7M0S1RoGBUb5xdpg=/kisspng-react-javascript-angularjs-ionic-atom-5b154be6947457.3471941815281223426081.png" alt="React Logo"/>
         </div>
     </div>
   )
